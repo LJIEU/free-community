df8f25a567cab9609f4f2c8f86f92a7a45430d07
JIE <2353471003@qq.com>
2022-10-24 14:57:45 +0800

    Initial commit

--- src/main/java/com/cjxjie/top/common/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/cjxjie/top/common/exception/ExceptionControllerAdvice.java
@@ -2,7 +2,9 @@ package com.cjxjie.top.common.exception;
 
 import com.cjxjie.top.common.utils.R;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
 /**
@@ -11,8 +13,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
  * @Date: 2022/7/10 20:40
  */
 @Slf4j
-//@ControllerAdvice(basePackages = "com.cjxjie.top")
-//@RestControllerAdvice // 统一异常处理
+@ControllerAdvice(basePackages = "com.cjxjie.top")
+@RestControllerAdvice // 统一异常处理
 public class ExceptionControllerAdvice {
 
     @ExceptionHandler(value = Exception.class)
@@ -20,5 +22,11 @@ public class ExceptionControllerAdvice {
         log.error("捕获异常信息:");
         System.out.println(e.getMessage());
         return R.error().put("error", e.getMessage());
+    }
+    @ExceptionHandler(value = CustomizeException.class)
+    public R customizeException(Exception e) {
+        log.error("捕获异常信息:");
+        System.out.println(e.getMessage());
+        return R.error().put("402", e.getMessage());
     }
 }
